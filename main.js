3fb216a490fe1208c3403b2c26f767803fb29c20
simhana <daum_c0m@naver.com>
2024-03-18 09:49:48 +0900

    listen

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+const { error } = require('console');
 var http = require('http');
 var url = require('url');
-var app = http.createServer(function(req, res)
+var app = http.createServer(function(req, res) //app: 서버의 객체 req: 요청 res: 응답
 {
     var queryData = url.parse(req.url, true).query;
     console.log(queryData);
-    res.end(queryData.id);
+
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'}); //오타나면 정보 전달이 제대로 되지 않음, 없으면 글자가 깨짐, 한 번만 써야함.
+    res.write('<h1>바보</h1>'); //여러번 작성 가능
+    res.end('<p>거짓말</p>'); //없으면 무한 로딩에 걸림, 한 번만 작성해야 함.
 });
-app.listen(3000);
+app.listen(3000); //서버를 3000번 포트에 연다.
+
+app.on('listening', ()=>{
+    console.log('3000번 포트에서 서버 실행 중')
+});
+
+app.on('error', (error)=>{
+    console.error(error);
+});
