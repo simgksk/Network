54162408b21046c71a83666ae1bdb6ae5326c49d
simhana <daum_c0m@naver.com>
2024-03-25 10:23:31 +0900

    undefine

--- main.js
+++ main.js
@@ -13,51 +13,91 @@ var app = http.createServer(function(req, res) //app: 서버의 객체 req: 요�
 
     if(pathName == '/')
     {
-        fs.readFile(`page/${title}`, 'utf-8', function(err, data)
+        // 메인페이지 
+        if(queryData.id == undefined)
         {
-        var template = `
-        <!DOCTYPE html>
-        <html lang="kr">
-        <head>
-            <meta charset="UTF-8">
-            <title>Web-204 ${queryData.id}</title>
-        </head>
-        <body>
-        <!-- 주석 제목 눌렀을 때 메인 페이지로 -->
-            <h1><a href="/">${title}</a></h1> 
-            <h2>${title} 시간표</h2>
+            var title = 'Welcome';
+            var data = '첫 번째 페이지';
+            var template = `
+            <!DOCTYPE html>
+            <html lang="kr">
+            <head>
+                <meta charset="UTF-8">
+                <title>Web-204 ${queryData.id}</title>
+            </head>
+            <body>
+            <!-- 주석 제목 눌렀을 때 메인 페이지로 -->
+                <h1><a href="/">${title}</a></h1> 
+                <h2>${title} 시간표</h2>
+            
+                <ol>
+                    <li><a href="/?id=list1">1교시</a></li>
+                    <li><a href="/?id=list2">2교시</a></li>
+                    <li><a href="/?id=list3">3교시</a></li>
+                </ol>
         
-            <ol>
-                <li><a href="/?id=list1">1교시</a></li>
-                <li><a href="/?id=list2">2교시</a></li>
-                <li><a href="/?id=list3">3교시</a></li>
-            </ol>
-    
-            <p> 
-                ${data}
-            </p>
-        
-        </body>
-        </html>
-        `;
-    
-        //console.log(queryData);
-    
-        //res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'}); //오타나면 정보 전달이 제대로 되지 않음, 없으면 글자가 깨짐, 한 번만 써야함.
-        //res.write('<h1>바보</h1>'); //여러번 작성 가능
-        //res.end(fs.readFileSync('index.html')); //없으면 무한 로딩에 걸림, 한 번만 작성해야 함.
-    
-        //res.writeHead(200, {'Content-Type': 'image/jpeg; charset=utf-8'});
-        //res.end(fs.readFileSync('home.jpg'));
-    
-        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        //res.end(fs.readFileSync('index.html'));
-    
-        //res.end(fs.readFileSync(__dirname+urlPath)); //요청한 url이 전달되면서 디렉토리에서 파일을 읽어와 응답
-        //console.log(__dirname+urlPath);
-    
-        res.end(template);
-        });
+                <p> 
+                    ${data}
+                </p>
+            
+            </body>
+            </html>
+            `;
+
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end(template);
+            
+        }
+        // 목차 페이지
+        else
+        {
+            fs.readFile(`page/${title}`, 'utf-8', function(err, data)
+            {
+                var title = data;
+                var template = `
+                <!DOCTYPE html>
+                <html lang="kr">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Web-204 ${queryData.id}</title>
+                </head>
+                <body>
+                <!-- 주석 제목 눌렀을 때 메인 페이지로 -->
+                    <h1><a href="/">${title}</a></h1> 
+                    <h2>${title} 시간표</h2>
+                
+                    <ol>
+                        <li><a href="/?id=list1">1교시</a></li>
+                        <li><a href="/?id=list2">2교시</a></li>
+                        <li><a href="/?id=list3">3교시</a></li>
+                    </ol>
+            
+                    <p> 
+                        ${data}
+                    </p>
+                
+                </body>
+                </html>
+                `;
+            
+                //console.log(queryData);
+            
+                //res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'}); //오타나면 정보 전달이 제대로 되지 않음, 없으면 글자가 깨짐, 한 번만 써야함.
+                //res.write('<h1>바보</h1>'); //여러번 작성 가능
+                //res.end(fs.readFileSync('index.html')); //없으면 무한 로딩에 걸림, 한 번만 작성해야 함.
+            
+                //res.writeHead(200, {'Content-Type': 'image/jpeg; charset=utf-8'});
+                //res.end(fs.readFileSync('home.jpg'));
+            
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+                //res.end(fs.readFileSync('index.html'));
+            
+                //res.end(fs.readFileSync(__dirname+urlPath)); //요청한 url이 전달되면서 디렉토리에서 파일을 읽어와 응답
+                //console.log(__dirname+urlPath);
+            
+                res.end(template);
+            });
+        }
     }
 
     else
