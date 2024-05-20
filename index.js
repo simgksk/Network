6463987250154ddf07aefa0f124b15e0ef82e775
simhana <daum_c0m@naver.com>
2024-05-20 09:20:03 +0900

    LogOut

--- index.js
+++ index.js
@@ -298,6 +298,12 @@ app.post('/process_login', (req, res)=>{
     }
 })
 
+app.get('/auth/logout', (req, res)=>{
+    req.session.destroy((err)=>{
+        res.redirect('/');
+    })
+})
+
 //404 Not Found 미들웨어
 app.use((req, res, next)=>{
     res.status(404).send('404 NOT FOUND');
