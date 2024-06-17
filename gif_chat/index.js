c8922275434375b1d174b2c72405227719a2e9d4
simhana <daum_c0m@naver.com>
2024-06-17 10:46:41 +0900

    setting

---
+++ gif_chat/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const session = require('express-session');
+const fileStore = require('session-file-store');
+const nunjucks = require('nunjucks');
+const webSocket = require('./socket');
+const port = 3001;
+
+dotenv.config();
+
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true
+})
+
+app.use(express({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new fileStore()
+}))
+
+app.get('/', (req, res)=>{
+    res.send('Hello world!');
+})
+
+const server = app.listen(port, ()=>{
+    console.log(`${port} 실행중`);
+})
+
+webSocket(server); //웹 소켓과 서버 연결
